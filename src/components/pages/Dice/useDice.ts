56bf62a0c10624808d9d1afd45f319b093067a3a
haitani <haitani@tam-tam.co.jp>
2024-05-12 15:59:48 +0900

    Update: 表示対象を角を削ったダイスへ変更

--- src/components/pages/Dice/useDice.ts
+++ src/components/pages/Dice/useDice.ts
@@ -8,30 +8,36 @@ export const useDice = () => {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
   const renderer = new THREE.WebGLRenderer()
 
+  const createDiceGeometry = () => {
+    const size = 1 // サイコロのサイズ
+    const roundness = 0.1 // 角の丸み
+    const geometry = new RoundedBoxGeometry(size, size, size, 10, roundness)
+    return geometry
+  }
+
   useEffect(() => {
-    // ジオメトリの設定
-    const geometry = new RoundedBoxGeometry(1, 1, 1, 10, 0.05)
+    // 環境光源を追加
+    const light = new THREE.AmbientLight(0xffffff)
+    scene.add(light)
 
-    // マテリアルの設定
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true })
-
-    // キューブを定義
-    const cube = new THREE.Mesh(geometry, material)
-
-    // シーンへキューブを追加
-    scene.add(cube)
+    // サイコロを追加
+    const diceGeometry = createDiceGeometry()
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+    const diceMesh = new THREE.Mesh(diceGeometry, material)
+    scene.add(diceMesh)
 
     // カメラの位置を設定
     camera.position.z = 5
 
     // アニメーション
     const animate = () => {
-      // XY方向に一定の角度を加算
       requestAnimationFrame(animate) // アニメーションを繰り返す
-      cube.rotation.x += 0.01
-      cube.rotation.y += 0.01
 
-      // レンダリング
+      // XY方向に一定の角度を加算
+      diceMesh.rotation.x += 0.01
+      diceMesh.rotation.y += 0.01
+
+      // シーンとカメラへレンダリング（この設定がなければ画面に何も表示されない）
       renderer.render(scene, camera)
     }
 
@@ -43,10 +49,8 @@ export const useDice = () => {
         scene.remove(child)
         if (child instanceof THREE.Mesh) {
           if (child.material) {
-            child.material.dispose() // マテリアルの破棄
-          }
-          if (child.geometry) {
-            child.geometry.dispose() // ジオメトリの破棄
+            child.material.dispose()
+            child.geometry.dispose()
           }
         }
       })
