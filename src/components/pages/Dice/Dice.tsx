23f935031481b7be10b09525e97e9a30a2c8c189
haitani <haitani@tam-tam.co.jp>
2024-05-12 22:04:06 +0900

    Update: ボタンで何度でも振り直せるようにする

--- src/components/pages/Dice/Dice.tsx
+++ src/components/pages/Dice/Dice.tsx
@@ -2,12 +2,14 @@ import { FC, useRef } from 'react'
 
 import { usePageTitle } from 'hooks/usePageTitle'
 
+import { Button } from 'components/parts/Button'
+
 import styles from './Dice.module.scss'
 import { useDice } from './useDice.ts'
 
 export const Dice: FC = () => {
   const canvas = useRef<HTMLCanvasElement>(null)
-  const { result } = useDice({ canvas })
+  const { result, throwDice } = useDice({ canvas })
 
   usePageTitle(`Dice`)
 
@@ -15,6 +17,7 @@ export const Dice: FC = () => {
     <>
       <div className={styles.canvas}>
         <p className={styles.result}>{result}</p>
+        <Button onClick={throwDice}>Roll</Button>
         <canvas ref={canvas} id="canvas"></canvas>
       </div>
     </>
